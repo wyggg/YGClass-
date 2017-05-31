a4612b67275c157d0d866fa3a49a3532ef0b00a7
wugang <773678819@qq.com>
2017-05-31 13:42:18 +0800

    修改transparentColor:(CGFloat)transparent;方法 增加colorRGB方法

--- YGClassCollection/YG++/UIColor+YGTool.m
+++ YGClassCollection/YG++/UIColor+YGTool.m
@@ -140,9 +140,33 @@
     return [self colorWithHexString:color alpha:1.0f];
 }
 
+//修改透明度
 - (UIColor *)transparentColor:(CGFloat)transparent{
-    const CGFloat *components = CGColorGetComponents(self.CGColor);
-    return [UIColor colorWithRed:components[0] green:components[1] blue:components[2] alpha:transparent];
+    NSDictionary *rgbs = [self colorRGB];
+    return [UIColor colorWithRed:[rgbs[@"R"] floatValue]
+                           green:[rgbs[@"G"] floatValue]
+                            blue:[rgbs[@"B"] floatValue] alpha:transparent];
+}
+
+//获得颜色RGB的值
+- (NSDictionary *)colorRGB{
+    
+    CGFloat components[3];
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char resultingPixel[4];
+    CGContextRef context = CGBitmapContextCreate(&resultingPixel,1,1,8,4,rgbColorSpace,(CGBitmapInfo)kCGImageAlphaNoneSkipLast);
+    CGContextSetFillColorWithColor(context, [self CGColor]);
+    CGContextFillRect(context, CGRectMake(0, 0, 1, 1));
+    CGContextRelease(context);
+    CGColorSpaceRelease(rgbColorSpace);
+    for (int component = 0; component < 3; component++) {
+        components[component] = resultingPixel[component] / 255.0f;
+    }
+    
+    return @{@"R":@(components[0]),
+             @"G":@(components[1]),
+             @"B":@(components[2])};
 }
 
 
